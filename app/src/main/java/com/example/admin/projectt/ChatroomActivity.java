a1dad8c85ed6d17c77acb5bc31cca6f28039d45c
imo-ininder <tony840622@gmail.com>
2016-08-02 02:48:38 +0800

    新增側邊攔

--- app/src/main/java/com/example/admin/projectt/ChatroomActivity.java
+++ app/src/main/java/com/example/admin/projectt/ChatroomActivity.java
@@ -1,20 +1,29 @@
 package com.example.admin.projectt;
 
+import android.app.Fragment;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v4.widget.*;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+import android.widget.ListView;
 import com.firebase.client.ChildEventListener;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
+import android.os.Build;
+import android.support.v7.app.ActionBarDrawerToggle;
+
 
 public class ChatroomActivity extends AppCompatActivity implements ChatConstant,Constant {
     private Firebase ref ,chatRef;
@@ -23,11 +32,63 @@ public class ChatroomActivity extends AppCompatActivity implements ChatConstant,
     TextView tvTitle,tvContent;
     Button chat,confirm,cancel;
     String myId;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private CharSequence mDrawerTitle;
+    private CharSequence mTitle;
+    Toolbar toolbar;
+    ActionBarDrawerToggle mDrawerToggle;
+    ObjectDrawerItem[] drawerItem = new ObjectDrawerItem[9];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chatroom);
         Firebase.setAndroidContext(this);
+        mTitle = mDrawerTitle = getTitle();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        this.setSupportActionBar(this.toolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+        //新增側邊欄裡的選項
+        drawerItem[0] = new ObjectDrawerItem("");
+        drawerItem[1] = new ObjectDrawerItem("放棄");
+        drawerItem[2] = new ObjectDrawerItem("協議取消");
+        drawerItem[3] = new ObjectDrawerItem("完成");
+        drawerItem[4] = new ObjectDrawerItem(" ");
+        drawerItem[5] = new ObjectDrawerItem(" ");
+        drawerItem[6] = new ObjectDrawerItem("拍照");
+        drawerItem[7] = new ObjectDrawerItem("本機相簿");
+        drawerItem[8] = new ObjectDrawerItem("google 相簿");
+
+        DrawerItemCustomAdapter adapter = new DrawerItemCustomAdapter(this, R.layout.listview_item_row, drawerItem);
+        mDrawerList.setAdapter(adapter);
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                mDrawerLayout,
+                toolbar,
+                R.string.drawer_open,
+                R.string.drawer_close
+        ) {
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+
 
         //initialization Firebase root url & Chat SharedPreferences
         chatData = getSharedPreferences(CHAT_SHAREDPREFERENCES,0);
@@ -127,4 +188,67 @@ public class ChatroomActivity extends AppCompatActivity implements ChatConstant,
             public void onCancelled(FirebaseError firebaseError) {}
         });
     }
+
+    //判斷選了哪一個選項
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItem(position);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void setTitle(CharSequence title) {
+
+    }
+
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+
+    //判斷選了哪一個選項(case的數字是drawerItem陣列的索引)
+    private void selectItem(int position) {
+
+        switch (position) {
+            case 0:                 //選了第一個選項(我讓他空者因為我覺得太上面)
+                Toast toast = Toast.makeText(ChatroomActivity.this,
+                        "Hello world!", Toast.LENGTH_LONG);
+
+                toast.show();
+                break;
+            case 1:                 //選了第2個選項(放棄)
+                Toast toast1 = Toast.makeText(ChatroomActivity.this,
+                        "放棄", Toast.LENGTH_LONG);
+
+                toast1.show();
+                break;
+            case 2:                 //選了第2個選項(協議取消)
+                Toast toast2 = Toast.makeText(ChatroomActivity.this,
+                        "協議取消", Toast.LENGTH_LONG);
+
+                toast2.show();
+                break;
+
+            default:
+                break;
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/admin/projectt/DrawerItemCustomAdapter.java
@@ -0,0 +1,42 @@
+package com.example.admin.projectt;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by user on 2016/7/28.
+ */
+public class DrawerItemCustomAdapter extends ArrayAdapter<ObjectDrawerItem> {
+    Context mContext;
+    int layoutResourceId;
+    ObjectDrawerItem data[] = null;
+
+    public DrawerItemCustomAdapter(Context mContext, int layoutResourceId, ObjectDrawerItem[] data) {
+
+        super(mContext, layoutResourceId, data);
+        this.layoutResourceId = layoutResourceId;
+        this.mContext = mContext;
+        this.data = data;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View listItem = convertView;
+
+        LayoutInflater inflater = ((Activity) mContext).getLayoutInflater();
+        listItem = inflater.inflate(layoutResourceId, parent, false);
+        TextView textViewName = (TextView) listItem.findViewById(R.id.textViewName);
+        ObjectDrawerItem folder = data[position];
+        textViewName.setText(folder.name);
+
+        return listItem;
+    }
+
+
+}
